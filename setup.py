1efdf7cb8e710f51bfbc203404e80d44373e8a72
Olav Morken <olavmrk@gmail.com>
2016-01-05 17:12:47 +0100

    Only depend on mock library for Python < 3.3.
    The mock library was added to Python in version 3.3, so we don't need to
    manually install it after that version.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+import sys
 from setuptools import setup, find_packages
 
 VERSION = '0.0'
+
+tests_require = [ ]
+if sys.version_info < (3, 3):
+    tests_require.append('mock') # mock became available as unittest.mock from Python 3.3.
 
 setup(
     name = 'ioctl',
@@ -12,6 +17,6 @@ setup(
     maintainer_email = 'olavmrk@gmail.com',
     url = 'https://github.com/olavmrk/python-ioctl',
     download_url = 'https://github.com/olavmrk/python-ioctl/tarball/v{version}'.format(version=VERSION),
-    tests_require = [ 'mock' ],
+    tests_require = tests_require,
     test_suite = 'tests',
 )
