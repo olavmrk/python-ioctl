1ee10c5c0c9c66e8ca3f1993d775f4b25bef50bc
Olav Morken <olavmrk@gmail.com>
2016-01-13 21:01:18 +0100

    test_linux: Add helper function for testing arch ioctl values.
    To make it easier to add tests for multiple architectures, add
    a helper function for testing with a given architecture.

--- tests/test_linux.py
+++ tests/test_linux.py
@@ -8,24 +8,36 @@ except ImportError:
 
 import ioctl.linux
 
+arch_values = {
+    'i386': {
+        'sizeof_int': 4,
+        'sizeof_ff_effect': 44,
+        'EVIOCSFF': 0x402c4580,
+        'BLKRRPART': 0x0000125f,
+        'RNDGETENTCNT': 0x80045200,
+        'RNDADDTOENTCNT': 0x40045201,
+        'FIFREEZE': 0xc0045877,
+    },
+}
 
 class TestLinux(unittest.TestCase):
 
-    @mock.patch('platform.machine', return_value='i386')
-    def test_i386(self, _mock):
-
-        sizeof_ff_effect = 44
-        EVIOCSFF = ioctl.linux.IOC('w', 'E', 0x80, sizeof_ff_effect)
-        self.assertEqual(EVIOCSFF, 0x402c4580)
+    def _test_values(self, values):
+        EVIOCSFF = ioctl.linux.IOC('w', 'E', 0x80, values['sizeof_ff_effect'])
+        self.assertEqual(EVIOCSFF, values['EVIOCSFF'])
 
         BLKRRPART = ioctl.linux.IO(0x12, 95)
-        self.assertEqual(BLKRRPART, 0x0000125f)
+        self.assertEqual(BLKRRPART, values['BLKRRPART'])
 
-        RNDGETENTCNT = ioctl.linux.IOR('R', 0x00, ctypes.c_int)
-        self.assertEqual(RNDGETENTCNT, 0x80045200)
+        RNDGETENTCNT = ioctl.linux.IOR('R', 0x00, values['sizeof_int'])
+        self.assertEqual(RNDGETENTCNT, values['RNDGETENTCNT'])
 
-        RNDADDTOENTCNT = ioctl.linux.IOW('R', 0x01, ctypes.c_int)
-        self.assertEqual(RNDADDTOENTCNT, 0x40045201)
+        RNDADDTOENTCNT = ioctl.linux.IOW('R', 0x01, values['sizeof_int'])
+        self.assertEqual(RNDADDTOENTCNT, values['RNDADDTOENTCNT'])
 
-        FIFREEZE = ioctl.linux.IOWR('X', 119, ctypes.c_int)
-        self.assertEqual(FIFREEZE, 0xc0045877)
+        FIFREEZE = ioctl.linux.IOWR('X', 119, values['sizeof_int'])
+        self.assertEqual(FIFREEZE, values['FIFREEZE'])
+
+    def test_i386(self):
+        with mock.patch('platform.machine', return_value='i386'):
+            self._test_values(arch_values['i386'])
