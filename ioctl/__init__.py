6a9dcbebd3535dc4dfe8e4b1d817b347f3c1c320
Olav Morken <olavmrk@gmail.com>
2016-02-03 18:35:26 +0100

    Remove the original helper functions.
    Now that we have the function-generating functions, I don't think the
    original functions make much sense.

--- ioctl/__init__.py
+++ ioctl/__init__.py
@@ -1,29 +1,12 @@
 import ctypes
 import ctypes.util
-import fcntl
 import os
-import sys
 
 from ._paramcheck import (
     check_ctypes_datatype,
     check_fd,
     check_request,
 )
-
-# In Python 2, the bytearray()-type does not support the buffer interface,
-# and can therefore not be used in ioctl().
-# This creates a couple of helper functions for converting to and from 
-if sys.version_info < (3, 0):
-    import array
-    def _to_bytearray(value):
-        return array.array('B', value)
-    def _from_bytearray(value):
-        return value.tostring()
-else:
-    def _to_bytearray(value):
-        return bytearray(value)
-    def _from_bytearray(value):
-        return bytes(value)
 
 _ioctl_fn = None
 def _get_ioctl_fn():
@@ -225,65 +208,3 @@ def ioctl_fn_w(request, datatype):
         value = datatype(value)
         ioctl(fd, request, value)
     return fn
-
-def ioctl_ptr_int(fd, request, value=0):
-    """Call ioctl() with an ``int *`` argument.
-
-    :param fd: File descriptor to operate on.
-    :param request: The ioctl request to call.
-    :param value: Optional value to pass to the ioctl() operation. Defaults to 0.
-    :return: Tuple of ``(ioctl_return, updated_value)``.
-            ``ioctl_return`` is the return value of the ioctl()-call, while
-            ``updated_value`` is the value of the integer argument after the
-            ioctl()-call.
-    """
-    check_fd(fd)
-    check_request(request)
-    res = ctypes.c_int(value)
-    ioctl_return = fcntl.ioctl(fd, request, res)
-    return (ioctl_return, res.value)
-
-def ioctl_ptr_size_t(fd, request, value=0):
-    """Call ioctl() with a ``size_t *`` argument.
-
-    :param fd: File descriptor to operate on.
-    :param request: ioctl request to call.
-    :param value: Optional value to pass to the ioctl() operation. Defaults to 0.
-    :return: Tuple of ``(ioctl_return, updated_value)``.
-             ``ioctl_return`` is the return value of the ioctl()-call, while
-             ``updated_value`` is the value of the size_t argument after the
-             ioctl()-call.
-    """
-    check_fd(fd)
-    check_request(request)
-    res = ctypes.c_size_t(value)
-    ioctl_return = fcntl.ioctl(fd, request, res)
-    return (ioctl_return, res.value)
-
-def ioctl_ptr_buffer(fd, request, value=None, length=None):
-    """Call ioctl() with a ``void *`` argument.
-
-    You must specify either the ``value`` parameter or the ``length`` parameter.
-    If the ``length`` parameter is specified, this function will allocate a byte
-    buffer of the specified length to pass to ioctl().
-
-    :param fd: File descriptor to operate on.
-    :param request: ioctl request to call.
-    :param value: Optional contents of the byte buffer at the start of the call.
-    :param length: Optional length of the byte buffer.
-    :return: Tuple of ``(ioctl_return, updated_value)``.
-             ``ioctl_return`` is the return value of the ioctl()-call, while
-             ``updated_value`` is the contents of the buffer after the ioctl()-call.
-    """
-    check_fd(fd)
-    check_request(request)
-    if value is None and length is None:
-        raise ValueError('Must specify either `value` or `length`')
-    if value is not None and length is not None:
-        raise ValueError('Cannot specify both `value` and `length`')
-    if value is None:
-        value = [0] * length
-    data = _to_bytearray(value)
-    ioctl_return = fcntl.ioctl(fd, request, data)
-    data = _from_bytearray(data)
-    return (ioctl_return, data)
--- tests/test_ioctl.py
+++ tests/test_ioctl.py
@@ -1,6 +1,4 @@
-import array
 import ctypes
-import fcntl
 import unittest
 
 import ioctl
@@ -9,71 +7,6 @@ try:
     import unittest.mock as mock
 except ImportError:
     import mock
-
-def _ctype_to_ptr(value):
-    ptr = ctypes.byref(value)
-    ptr_type = ctypes.POINTER(ctypes.c_ubyte)
-    return ctypes.cast(ptr, ptr_type)
-
-def _ctype_to_bytes(value):
-    value_length = ctypes.sizeof(value)
-    value_ptr = _ctype_to_ptr(value)
-    return bytes(bytearray(value_ptr[0:value_length]))
-
-class IoctlMock(mock.MagicMock):
-
-    def __init__(self, *args, **kwargs):
-        set_value = kwargs.pop('set_value', None)
-        if set_value is not None:
-            if isinstance(set_value, bytes):
-                pass # Nothing to convert here
-            elif isinstance(set_value, ctypes._SimpleCData):
-                set_value = _ctype_to_bytes(set_value)
-            else:
-                raise TypeError('`set_value` must be a `bytes` object or a ctypes type')
-        self.__dict__['_set_value'] = set_value
-        super(IoctlMock, self).__init__(*args, **kwargs)
-
-    @staticmethod
-    def _arg_to_bytes(arg):
-        if isinstance(arg, bytearray):
-            return bytes(arg)
-        elif isinstance(arg, array.array):
-            return arg.tostring()
-        elif isinstance(arg, int):
-            return _ctype_to_bytes(ctypes.c_int(arg))
-        elif isinstance(arg, ctypes._SimpleCData):
-            return _ctype_to_bytes(arg)
-        else:
-            raise TypeError('Unhandled type of arg-parameter for ioctl: {argtype}'.format(argtype=str(type(arg))))
-
-    def _set_arg_return(self, arg):
-        set_value = self.__dict__['_set_value']
-        if set_value is None:
-            return # Nothing to set
-
-        if isinstance(arg, bytearray) or isinstance(arg, array.array):
-            arg_array = arg
-            arg_size = len(arg)
-        elif isinstance(arg, ctypes._SimpleCData):
-            arg_array = _ctype_to_ptr(arg)
-            arg_size = ctypes.sizeof(arg)
-        else:
-            raise TypeError('Unhandled type of arg-parameter for ioctl return: {argtype}'.format(argtype=str(type(arg))))
-
-        ret_size = len(set_value)
-        if arg_size != ret_size:
-            raise Exception('fcntl.ioctl called with a {arg_size} byte buffer, but should have received a {ret_size} byte buffer.'.format(arg_size=arg_size, ret_size=ret_size))
-
-        for i, v in enumerate(bytearray(set_value)):
-            arg_array[i] = v
-
-    def __call__(self, fd, request, arg=0, mutate_flag=True):
-        wrapped_arg = IoctlMock._arg_to_bytes(arg)
-        ret = super(IoctlMock, self).__call__(fd=fd, request=request, arg=wrapped_arg, mutate_flag=mutate_flag)
-        self._set_arg_return(arg)
-        return ret
-
 
 class TestMain(unittest.TestCase):
 
@@ -151,47 +84,6 @@ class TestMain(unittest.TestCase):
 
         fn = ioctl.ioctl_fn_w(32, ctypes.c_int)
         fn(12, 42)
-
-    @mock.patch('fcntl.ioctl', new_callable=IoctlMock, set_value=ctypes.c_size_t(123), return_value=321)
-    def test_ioctl_ptr_size_t(self, ioctl_mock):
-        ret = ioctl.ioctl_ptr_size_t(5, 7)
-        self.assertEqual(ret, (321, 123))
-        kwargs = ioctl_mock.call_args[1]
-        self.assertEqual(kwargs['fd'], 5)
-        self.assertEqual(kwargs['request'], 7)
-        self.assertEqual(len(kwargs['arg']), ctypes.sizeof(ctypes.c_size_t))
-        self.assertEqual(kwargs['mutate_flag'], True)
-
-
-    @mock.patch('fcntl.ioctl', new_callable=IoctlMock, set_value=ctypes.c_int(123), return_value=321)
-    def test_ioctl_ptr_int(self, ioctl_mock):
-        ret = ioctl.ioctl_ptr_int(5, 7)
-        self.assertEqual(ret, (321, 123))
-        kwargs = ioctl_mock.call_args[1]
-        self.assertEqual(kwargs['fd'], 5)
-        self.assertEqual(kwargs['request'], 7)
-        self.assertEqual(len(kwargs['arg']), ctypes.sizeof(ctypes.c_int))
-        self.assertEqual(kwargs['mutate_flag'], True)
-
-    @mock.patch('fcntl.ioctl', new_callable=IoctlMock, set_value=b'\x01\x02', return_value=321)
-    def test_ioctl_ptr_buffer_value(self, ioctl_mock):
-        ret = ioctl.ioctl_ptr_buffer(5, 7, value=b'\x02\x01')
-        self.assertEqual(ret, (321, b'\x01\x02'))
-        kwargs = ioctl_mock.call_args[1]
-        self.assertEqual(kwargs['fd'], 5)
-        self.assertEqual(kwargs['request'], 7)
-        self.assertEqual(len(kwargs['arg']), 2)
-        self.assertEqual(kwargs['mutate_flag'], True)
-
-    @mock.patch('fcntl.ioctl', new_callable=IoctlMock, set_value=b'\x01\x02', return_value=321)
-    def test_ioctl_ptr_buffer_length(self, ioctl_mock):
-        ret = ioctl.ioctl_ptr_buffer(5, 7, length=2)
-        self.assertEqual(ret, (321, b'\x01\x02'))
-        kwargs = ioctl_mock.call_args[1]
-        self.assertEqual(kwargs['fd'], 5)
-        self.assertEqual(kwargs['request'], 7)
-        self.assertEqual(len(kwargs['arg']), 2)
-        self.assertEqual(kwargs['mutate_flag'], True)
 
 if __name__ == '__main__':
     unittest.main()
